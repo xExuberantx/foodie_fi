20019fecdda5dae9f313b33cba43ea84f22f4fad
Kamil <131042937+xExuberantx@users.noreply.github.com>
2023-04-27 10:08:30 +0200

    Update foodie_fi.sql

--- foodie_fi.sql
+++ foodie_fi.sql
@@ -1,16 +1,72 @@
 -- A. Customer Journey
+-- Based off the 8 sample customers provided in the sample from the subscriptions table, write a brief description about each customer’s onboarding journey.
+-- Try to keep it as short as possible - you may also want to run some sort of join to make your explanations a bit easier!
+/*
+Customer 1 downgraded to the basic version after the trial week expired.
+Customer 2 upgraded to the pro annual version after the trial week expired.
+Customer 11 cancelled his subscription after the trial.
+Customer 13 downgraded to the basic version after the trial and after 3 months upgraded to pro monthly.
+Customer 15 probably forgot to cancel his subscription as it switched to pro monthly and got cancelled a month later.
+Customer 16 downgraded to the basic version after the trial, but then upgraded to pro annual after 4 months.
+Customer 18 probably forgot to cancel his subscription as it switched to pro monthly.
+Customer 19 used pro monthly for 2 months after trial and upgraded to pro annual.
+*/
+SELECT
+    customer_id,
+    plan_name,
+    start_date
+FROM foodie_fi.subscriptions
+JOIN foodie_fi.plans
+USING(plan_id)
+WHERE customer_id IN (1, 2, 11, 13, 15, 16, 18, 19)
+ORDER BY customer_id;
 
 -- B. Data Analysis Questions
 
 -- 1. How many customers has Foodie-Fi ever had?
 
+SELECT
+    COUNT(DISTINCT customer_id),
+    MAX(customer_id)
+FROM foodie_fi.subscriptions;
+
 -- 2. What is the monthly distribution of trial plan start_date values for our dataset - use the start of the month as the group by value
+SELECT
+    DATE_PART('month', start_date) as month,
+    COUNT(*)
+FROM foodie_fi.subscriptions
+GROUP BY month
+ORDER BY count DESC;
 
 -- 3. What plan start_date values occur after the year 2020 for our dataset? Show the breakdown by count of events for each plan_name
 
+SELECT
+    plan_name,
+    COUNT(*)
+FROM foodie_fi.subscriptions
+JOIN foodie_fi.plans
+USING(plan_id)
+WHERE start_date > '2020-12-31'
+GROUP BY plan_name;
+-- No new trials after 2020
+
 -- 4. What is the customer count and percentage of customers who have churned rounded to 1 decimal place?
 
+SELECT
+    COUNT(DISTINCT customer_id) as cust_count,
+    (SELECT COUNT(customer_id) FROM foodie_fi.subscriptions WHERE plan_id = 4) as churned,
+    ROUND((SELECT COUNT(customer_id) FROM foodie_fi.subscriptions WHERE plan_id = 4)*100.0/COUNT(DISTINCT customer_id), 1) as churn_perc
+FROM foodie_fi.subscriptions
+JOIN foodie_fi.plans
+USING(plan_id)
+
 -- 5. How many customers have churned straight after their initial free trial - what percentage is this rounded to the nearest whole number?
+SELECT *
+FROM foodie_fi.subscriptions
+JOIN foodie_fi.plans
+USING(plan_id)
+ORDER BY customer_id
+--WHERE plan_id IN (0, 4)
 
 -- 6. What is the number and percentage of customer plans after their initial free trial?
 
